879de62e278118954266c0e02ca331f4a81db0cd
Elcio Lopes da Silva <elciols@gmail.com>
2022-07-30 19:50:12 -0300

    Freezed

---
+++ lib/features/bloc_example/bloc_freezed/example_freezed_bloc.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'example_freezed_bloc.freezed.dart';
+part 'example_freezed_event.dart';
+part 'example_freezed_state.dart';
+
+class ExampleFreezedBloc
+    extends Bloc<ExampleFreezedEvent, ExampleFreezedState> {
+  ExampleFreezedBloc() : super(ExampleFreezedState.initial()) {
+    on<_ExampleFreezedEventFindNames>(_findNames);
+    on<_ExampleFreezedEventAddName>(_addName);
+    on<_ExampleFreezedEventRemoveName>(_removeName);
+  }
+
+  FutureOr<void> _addName(
+    _ExampleFreezedEventAddName event,
+    Emitter<ExampleFreezedState> emit,
+  ) async {
+    final names = state.maybeWhen(
+      data: (names) => names,
+      orElse: () => const <String>[],
+    );
+
+    emit(
+      ExampleFreezedState.showBannner(
+          names: names, message: 'Aguarde, nome sendo inserido!!!'),
+    );
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+
+    final newNames = [...names];
+    newNames.add(event.name);
+
+    emit(ExampleFreezedState.data(names: newNames));
+  }
+
+  FutureOr<void> _removeName(
+    _ExampleFreezedEventRemoveName event,
+    Emitter<ExampleFreezedState> emit,
+  ) {
+    final names = state.maybeWhen(
+      data: (names) => names,
+      orElse: () => const <String>[],
+    );
+
+    final newNames = [...names];
+
+    newNames.retainWhere((element) => element != event.name);
+    emit(ExampleFreezedState.data(names: newNames));
+  }
+
+  FutureOr<void> _findNames(
+    _ExampleFreezedEventFindNames event,
+    Emitter<ExampleFreezedState> emit,
+  ) async {
+    emit(ExampleFreezedState.loading());
+    final names = [
+      'Elcinho',
+      'EtFlutter',
+      'Flutter',
+      'Dart',
+      'Flutter Bloc',
+    ];
+    await Future.delayed(const Duration(seconds: 4));
+    emit(ExampleFreezedState.data(names: names));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:adf_contact_bloc/features/bloc_example/bloc/example_bloc.dart';
 import 'package:adf_contact_bloc/features/bloc_example/bloc_example.dart';
+import 'package:adf_contact_bloc/features/bloc_example/bloc_freezed/example_freezed_bloc.dart';
+import 'package:adf_contact_bloc/features/bloc_example/bloc_freezed_example.dart';
 import 'package:adf_contact_bloc/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,6 +27,10 @@ class MyApp extends StatelessWidget {
               create: (_) => ExampleBloc()..add(ExampleFindNameEvent()),
               child: const BlocExample(),
             ),
+        '/bloc/example/freezed': (context) => BlocProvider(
+            create: (context) => ExampleFreezedBloc()
+              ..add(const ExampleFreezedEvent.findNames()),
+            child: const BlocFreezedExample()),
       },
     );
   }
